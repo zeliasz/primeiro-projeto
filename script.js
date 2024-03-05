17b5701754a282ac59f0394a07055d04137a9c79
zeliasz <elias.samuel.zapp.filho@gmial.com>
2024-03-05 00:46:09 -0300

    :tada: initial commit

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
+const img = document.querySelector("#profile img")
+
+if(html.classList.contains('light')) {
+img.setAttribute("src", "./assets/gengarbranco.png")
+} else {
+  img.setAttribute("src", "./assets/gengaar.png")
+} 
 }
