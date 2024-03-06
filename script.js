790f738f40b42b4de190bcef25e66b2eb5f23b56
zeliasz <elias.samuel.zapp.filho@gmial.com>
2024-03-05 22:01:23 -0300

    change in the button animation

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-const img = document.querySelector("#profile img")
+  const img = document.querySelector("#profile img")
 
-if(html.classList.contains('light')) {
-img.setAttribute("src", "./assets/gengarbranco.png")
-} else {
-  img.setAttribute("src", "./assets/gengar.png")
-} 
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/gengarbranco.png")
+    alt: "foto do pokemon gengar mega evoluido"
+  } else {
+    img.setAttribute("src", "./assets/gengar.png")
+    alt: "foto do pokemon gengar"
+  }
 }
